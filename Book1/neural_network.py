4bfc3e0e1dcef0a555bd74fd52b954891d2faee6
momeemt <momiimt@gmail.com>
2020-04-24 06:29:22 +0900

    ♻️ PEP8の規則に従ったコードに修正

--- Book1/neural_network.py
+++ Book1/neural_network.py
@@ -2,38 +2,44 @@ import numpy as np
 
 
 def init_network():
-  network = {}
-  network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-  network['b1'] = np.array([0.1, 0.2, 0.3])
-  network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-  network['b2'] = np.array([0.1, 0.2])
-  network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
-  network['b3'] = np.array([0.1, 0.2])
+    network = {
+      'W1': np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]),
+      'b1': np.array([0.1, 0.2, 0.3]),
+      'W2': np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]),
+      'b2': np.array([0.1, 0.2]),
+      'W3': np.array([[0.1, 0.3], [0.2, 0.4]]),
+      'b3': np.array([0.1, 0.2])
+    }
 
-  return network
+    return network
 
 
 def forward(network, x):
-  W1, W2, W3 = network['W1'], network['W2'], network['W3']
-  b1, b2, b3 = network['b1'], network['b2'], network['b3']
-  a1 = np.dot(x, W1) + b1
-  z1 = sigmoid(a1)
-  a2 = np.dot(z1, W2) + b2
-  z2 = sigmoid(a2)
-  a3 = np.dot(z2, W3) + b3
-  y = identify_function(a3)
-  return y
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identify_function(a3)
+    return y
 
 
 def identify_function(x):
-  return x
+    return x
 
 
 def sigmoid(x):
-  return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
-network = init_network()
-x = np.array([1.0, 0.5])
-y = forward(network, x)
-print(y)
+def main():
+    network = init_network()
+    x = np.array([1.0, 0.5])
+    y = forward(network, x)
+    print(y)
+
+
+if __name__ == "__main__":
+    main()
