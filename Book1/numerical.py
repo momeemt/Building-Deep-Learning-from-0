60551f3297f4aaeb6f4b05f73ee4a52014143a6a
momeemt <momiimt@gmail.com>
2020-04-25 06:59:11 +0900

    ✨ 勾配の実装(4.4)

--- Book1/numerical.py
+++ Book1/numerical.py
@@ -23,7 +23,28 @@ def function_tmp2(x1):
     return 3.0**2.0 + x1*x1
 
 
-print(numerical_diff(function_1, 5))
-print(numerical_diff(function_1, 10))
-print(numerical_diff(function_tmp1, 3.0))
-print(numerical_diff(function_tmp2, 4.0))
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+    return grad
+
+
+# print(numerical_diff(function_1, 5))
+# print(numerical_diff(function_1, 10))
+# print(numerical_diff(function_tmp1, 3.0))
+# print(numerical_diff(function_tmp2, 4.0))
+
+
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+print(numerical_gradient(function_2, np.array([3.0, 0.0])))
