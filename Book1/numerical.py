3821db71a53ab9e5fafadfe271c9ea691bc27708
momeemt <momiimt@gmail.com>
2020-04-25 06:54:06 +0900

    ✨ 偏微分の実装(4.3.3)

--- Book1/numerical.py
+++ Book1/numerical.py
@@ -11,13 +11,19 @@ def function_1(x):
     return 0.01*x**2 + 0.1*x
 
 
-x = np.arange(0.0, 20.0, 0.1)
-y = function_1(x)
-plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.plot(x, y)
-plt.show()
+def function_2(x):
+    return np.sum(x**2)
+
+
+def function_tmp1(x0):
+    return x0*x0 + 4.0**2.0
+
+
+def function_tmp2(x1):
+    return 3.0**2.0 + x1*x1
 
 
 print(numerical_diff(function_1, 5))
 print(numerical_diff(function_1, 10))
+print(numerical_diff(function_tmp1, 3.0))
+print(numerical_diff(function_tmp2, 4.0))
